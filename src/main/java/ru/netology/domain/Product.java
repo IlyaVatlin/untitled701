134d29008eeca92ee0f41d94294b1b4403924d38
IlyaVatlin <107053785+IlyaVatlin@users.noreply.github.com>
2022-08-31 20:31:19 +0300

    Update Product.java

--- src/main/java/ru/netology/domain/Product.java
+++ src/main/java/ru/netology/domain/Product.java
@@ -1,4 +1,28 @@
-package domain;
+package ru.netology.domain;
 
 public class Product {
+    int id;
+    String title;
+    int price;
+
+    public boolean matches(String search) {
+        if (this.getTitle().equalsIgnoreCase(search)) {
+            return true;
+        }
+        return false;
+    }
+
+    public Product(int id, String title, int price) {
+        this.id = id;
+        this.title = title;
+        this.price = price;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public int getId() {
+        return id;
+    }
 }
