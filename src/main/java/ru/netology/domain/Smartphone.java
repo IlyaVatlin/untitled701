9cd2384fba882052db22b95beeb1749207eda581
IlyaVatlin <107053785+IlyaVatlin@users.noreply.github.com>
2022-08-31 20:31:40 +0300

    Update Smartphone.java

--- src/main/java/ru/netology/domain/Smartphone.java
+++ src/main/java/ru/netology/domain/Smartphone.java
@@ -1,4 +1,25 @@
-package domain;
+package ru.netology.domain;
 
-public class Smartphone {
+public class Smartphone extends Product {
+    String manufacturer;
+
+    public Smartphone(int id, String title, int price, String manufacturer) {
+        super(id, title, price);
+        this.manufacturer = manufacturer;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    @Override
+    public boolean matches(String search) {
+        if (super.matches(search)) {
+            return true;
+        }
+        if (this.getManufacturer().equalsIgnoreCase(search)) {
+            return true;
+        }
+        return false;
+    }
 }
